00cea527c8501984d13afd62c328481b6a25b231
dima <dimafrunza69@gmail.com>
2023-09-06 08:33:15 +0200

    Semplice websocket tra vari client

--- app/game/page.tsx
+++ app/game/page.tsx
@@ -1,3 +1,38 @@
-export default function Page() {
-  return <div> Questa è la pagina di gioco </div>
+"use client" 
+
+function Home(){
+  const socket = new WebSocket('ws://localhost:8080');
+
+  socket.addEventListener('open', (event) => {
+      console.log('WebSocket connection opened:');
+      });
+
+  socket.addEventListener('message', (event) => {
+      console.log('Received message:', event.data);
+      });
+
+  socket.addEventListener('close', (event) => {
+      console.log('WebSocket connection closed:');
+      });
+
+  socket.addEventListener('error', (event) => {
+      console.error('WebSocket error:');
+      });
+
+  const gotem = (msg: string) => {
+    socket.send(msg);
+  }
+
+  return (
+  <div>
+      Domanda 
+      <div onClick={ () => gotem('giacomo') }>opzione 1</div>
+      <div>opzione 2</div>
+      <div>opzione 3</div>
+      <div>opzione 4</div>
+  </div>
+  )
+
 }
+
+export default Home;
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,42 @@
 "use client";
-import { io } from "socket.io-client";
+import { useState } from "react";
 
-const socket = io("ws://localhost:3000");
+export default function Page() {
+  const [counter, setCounter] = useState(0);
 
-// send a message to the server
-socket.emit("hello from client", 5, "6", { 7: Uint8Array.from([8]) });
+  const increase = () => {
+    setCounter(count => count + 1);
+  };
 
-// receive a message from the server
-socket.on("hello from server", (...args) => {
-  // ...
-});
+  const socket = new WebSocket('ws://localhost:8080');
+
+  socket.addEventListener('open', (event) => {
+      console.log('WebSocket connection opened:');
+      });
+
+  socket.addEventListener('message', (event) => {
+      console.log('Received message:', event.data);
+        if (event.data === 'giacomo') {
+          increase();
+          socket.send('risposta giusta');
+          socket.close();
+        }
+      });
+
+  socket.addEventListener('close', (event) => {
+      console.log('WebSocket connection closed:');
+      });
+
+  socket.addEventListener('error', (event) => {
+      console.error('WebSocket error:');
+      });
+
+  return(
+      <div>
+          <span>
+              {counter}
+          </span>
+      </div>
+      )
+
+}
